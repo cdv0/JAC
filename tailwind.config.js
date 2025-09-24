6a0f0703e6765a21d03df38d8a6865f5bc3e4c3f
William Shen <william.shen.0001@gmail.com>
2025-09-24 11:55:49 -0700

    Fixed all errors involving the project base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
