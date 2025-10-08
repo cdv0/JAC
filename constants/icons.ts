ebd2eaf78e607d9f8f1d94d2a4a9911adaafefff
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-08 14:54:34 -0700

    Individual images for highlighted and non highligted icons

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,13 +1,21 @@
 import garage from "@/public/assets/icons/garage-icon.svg"
+import garageH from "@/public/assets/icons/garageH-icon.svg"
 import home from "@/public/assets/icons/home-icon.svg"
 import map from "@/public/assets/icons/map-icon.svg"
+import mapH from "@/public/assets/icons/mapH-icon.svg"
 import profile from "@/public/assets/icons/profile-icon.svg"
+import profileH from "@/public/assets/icons/profileH-icon.svg"
 import search from "@/public/assets/icons/search-icon.svg"
+import seachH from "@/public/assets/icons/searchH-icon.svg"
 
 export const icons = {
     garage,
+    garageH,
     home,
     map,
+    mapH,
     profile,
-    search
+    profileH,
+    search,
+    seachH
 };
