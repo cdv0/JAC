75d1e184ef694f35a989c43ce64bca8b2952e45d
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-15 10:59:32 -0700

    initial layout of the mechanic page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import NormalButton from "../components/NormalButton";
 import SearchBar from "../components/SearchBar";
-
-
 export default function Index() {
   return (
-    <View
-      className="flex-1 justify-center items-center"
-    >
-      <Text >Test</Text>
-      <SearchBar placeholder1="Search" placeholder2="Location"/>
-      <Text className="flex-1">Test</Text>
-    </View>
+   <SafeAreaView className="flex-1" edges={['right', 'top', 'left']}>
+      <View
+      className="flex-1 bg-white relative"
+      >
+        <View className="justify-center w-full h-[150px] relative">
+          <images.searchBackground width="100%" height="100%" style={{ position: 'absolute', top:0, zIndex: 0 }} />
+          <View className="ml-[50%]">
+            <NormalButton onClick={()=>{}} text={"Enter Garage"}/>
+          </View>
+        </View > 
+        <SearchBar placeholder1="Search" placeholder2="Location"/>
+
+      </View>
+   </SafeAreaView>
+    
   );
 }
