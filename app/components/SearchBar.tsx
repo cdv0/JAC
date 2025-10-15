70e9af0b405818b10533b5b8ac79777a40c254c6
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-15 12:15:48 -0700

    Minor adjusments

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -14,7 +14,7 @@ interface Props{
 
 const SearchBar = ({placeholder1, value1, onChangeText1, placeholder2, value2,  onChangeText2}:Props) => {
   return (
-    <View className='border border-stroke flex-row items-center bg-white rounded-full px-2 py-2 my-4 mx-5'>
+    <View className='border border-stroke flex-row items-center bg-white rounded-full px-2 py-1 my-4 mx-5'>
       {/*General search*/}
       <icons.search width={55} height={25}/>
       <TextInput
@@ -25,7 +25,7 @@ const SearchBar = ({placeholder1, value1, onChangeText1, placeholder2, value2,  
         className="flex-1 pr-20 text-textBlack"  
       />
       {/* Diveder*/}
-      <View className="w-0.5 h-full bg-stroke mx-0.5"/> 
+      <View className="w-0.5 h-full bg-stroke"/> 
       {/* Location search*/}
       <icons.location width={55} height={25}/>
       <TextInput
