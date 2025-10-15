65e3046ef92b9944bb348652aa37ad437a6a3812
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-15 10:57:55 -0700

    minor adjustments for searchbar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -5,40 +5,35 @@ import { TextInput, View } from 'react-native';
 interface Props{
     placeholder1 :string;
     value1?: string;
-    onPress1?: () =>void;
     onChangeText1?: (text: string) => void;
     placeholder2 :string;
     value2?: string;
-    onPress2?: () =>void;
     onChangeText2?: (text: string) => void;
-
 
 }
 
-const SearchBar = ({placeholder1, value1, onPress1, onChangeText1, placeholder2, value2, onPress2, onChangeText2}:Props) => {
+const SearchBar = ({placeholder1, value1, onChangeText1, placeholder2, value2,  onChangeText2}:Props) => {
   return (
-    <View className='border border-stroke flex-row items-center bg-white rounded-full px-2 py-2 my-4'>
+    <View className='border border-stroke flex-row items-center bg-white rounded-full px-2 py-2 my-4 mx-5'>
       {/*General search*/}
-      <icons.search className='w-21 h-full' color = "black"/>
+      <icons.search width={55} height={25}/>
       <TextInput
-        onPress = {onPress1}
         placeholder={placeholder1}
         value ={value1}
         onChangeText={onChangeText1}
         placeholderTextColor= "#9E9E9E"
-        className="flex-1 ml-5 pr-40 text-textBlack"  
+        className="flex-1 pr-20 text-textBlack"  
       />
       {/* Diveder*/}
-      <View className="w-0.5 h-full bg-stroke mx-5"/> 
+      <View className="w-0.5 h-full bg-stroke mx-0.5"/> 
       {/* Location search*/}
-      <icons.location className='w-21 h-full'/>
+      <icons.location width={55} height={25}/>
       <TextInput
-        onPress = {onPress2}
         placeholder={placeholder2}
         value ={value2}
         onChangeText={onChangeText2}
         placeholderTextColor= "#9E9E9E"
-        className="flex-1 ml-5 mr-5 text-textBlack"  
+        className="flex-1 text-textBlack"  
       />
     
     </View>
