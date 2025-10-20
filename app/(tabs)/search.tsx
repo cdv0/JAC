3d94f2a5088bb9ecb968006c79cabf1768d700e0
William Shen <william.shen.0001@gmail.com>
2025-10-20 11:13:41 -0700

    added use Effect for search and layout

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View, ScrollView } from 'react-native'
 import NormalButton from '../components/NormalButton'
 
+
 const search = () => {
+
+    const [mechanics, setMechanics] = useState([])
+    useEffect(() =>{
+        const data = async () => {
+           try{
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const json = await response.json();
+            setMechanics(json);
+           } catch (error){
+            console.error(error);
+           }   
+           
+        }
+        //call
+        data();
+    }, []);
+
+
   return (
     <View className= "flex flex-col">
       
@@ -13,7 +32,14 @@ const search = () => {
             <NormalButton text={'Oil Change'} size={'28'} />
             <NormalButton text={'Tire Rotation'} size={'28'} />
         </ScrollView>
-
+        <View>
+            {mechanics.map((item) => (
+                <View key={item.id} className = "border-2 border-black m-2 p-2">
+                    <Text className = "font-bold">{item.name}</Text>
+                </View>
+            }         
+            
+        </View>
         <View> This is where the dealers and mechanics will go</View>
 
     </View>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,9 @@
-import { Amplify, ResourcesConfig } from 'aws-amplify'
-import { Stack } from 'expo-router'
-import '../global.css'
-
-// || Amplify Cognito & Backend Connection ||
-const authConfig: ResourcesConfig['Auth'] = {
-  Cognito: {
-    userPoolId: 'us-west-2_Uejqr4LnW',
-    userPoolClientId: '3r4dhpaqdrq1ap7lcih1lp9t62',
-    identityPoolId: 'us-west-2:b8d8a775-c847-4198-85b0-d7000c2694ca',
-    signUpVerificationMethod: 'code',
-    loginWith: {
-      email: true,
-      phone: false,
-    },
-  },
-}
-Amplify.configure({ Auth: authConfig })
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name = "(tabs)" options = {{headerShown: false}}/>
     </Stack>
-  )
+  );
 }
