5dda6397a8c4b730ed4faf232b711c1d90d8abf3
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-21 20:38:28 -0700

    Min price, max price, and slider value only gets preserved when apply button is clicked. Otherwise gets reset to prevous value when X is clicked

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,27 +17,32 @@ export default function Index() {
   const [isTransmissionActive, setIsTransmissionActive] = useState(false);
   const [isWheelActive, setIsWheelActive] = useState(false);
 
-  const [isServicesActive, setisServicesActive] = useState(false);
+  const [isServicesActive, setIsServicesActive] = useState(false);
 
-  const [isRelevanceActive, setisRelevanceActive] = useState(false);
-  const [isRelevanceApplied, setisRelevanceApplied] = useState(false);
+  const [isRelevanceActive, setIsRelevanceActive] = useState(false);
+  const [isRelevanceApplied, setIsRelevanceApplied] = useState(false);
 
-  const [isOpenNowActive, setisOpenNowActive] = useState(false);
-  const [isOpenNowApplied, setisOpenNowApplied] = useState(false);
+  const [isOpenNowActive, setIsOpenNowActive] = useState(false);
+  const [isOpenNowApplied, setIsOpenNowApplied] = useState(false);
 
-  const [isPopularActive, setisPopularActive] = useState(false); 
-  const [isPopularApplied, setisPopularApplied] = useState(false); 
+  const [isPopularActive, setIsPopularActive] = useState(false); 
+  const [isPopularApplied, setIsPopularApplied] = useState(false); 
   
-  const [isRatingActive, setisRatingActive] = useState(false);
-  const [isRatingApplied, setisRatingApplied] = useState(false);
+  const [isRatingActive, setIsRatingActive] = useState(false);
+  const [isRatingApplied, setIsRatingApplied] = useState(false);
   
   const [minP, setminP] = useState('');
+  const [tempMinP, setTempMinP] = useState('');
+
   const [maxP, setmaxP] = useState('');
+  const [tempMaxP, setTempMaxP] = useState('');
+
   const width= '45%';
   const minD = 0;
   const maxD = 20;
   
   const [sliderValue, setSliderValue] = useState(maxD / 2); 
+  const [tempSliderValue, setTempSliderValue] = useState(sliderValue);
 
   return (
    <SafeAreaView className="flex-1" edges={['right', 'top', 'left']}>
@@ -54,7 +59,7 @@ export default function Index() {
         <View >
           <ScrollView  horizontal={true} contentContainerStyle={{gap:10}} showsHorizontalScrollIndicator={false}>
             <NormalButton variant={`${isFiltersActive?`primary`:`outline`}`} onClick={()=>{setisFiltersModal(!isFiltersModal)}} text="Filters"/>
-            <NormalButton variant={`${isServicesActive?`primary`:`outline`}`} onClick={()=>{setisServicesActive(!isServicesActive)}} text="Services"/>
+            <NormalButton variant={`${isServicesActive?`primary`:`outline`}`} onClick={()=>{setIsServicesActive(!isServicesActive)}} text="Services"/>
             <ToggleButton flag = {isOilChangeActive} onPress={(newF)=>{setIsOiltChangeActive(newF)}} text="Oil Change" />
             <ToggleButton flag = {isTireActive} onPress={(newf)=>{setIsTireActive(newf)}} text="Tire Rotation"/>
             <ToggleButton flag = {isSmogActive} onPress={(newf)=>{setIsSmogActive(newf)}} text="Smog Check"/>
@@ -68,7 +73,7 @@ export default function Index() {
         </View>
 
         {/*Expand filters */}
-        <Modal visible={isFiltersModal}>
+        <Modal visible={isFiltersModal} >
           <View className="flex-1">
               <View className="flex-row justify-between ml-[2%] mr-[2%] mt-[5%] mb-[5%]">
                   <Text className="justify-start  text-[25px] buttonTextBlack">
@@ -78,86 +83,87 @@ export default function Index() {
                   <Pressable 
                     onPress={()=> 
                       {
-                        setisRelevanceActive(isRelevanceApplied);
-                        setisOpenNowActive(isOpenNowApplied);
-                        setisPopularActive(isPopularApplied)
-                        setisRatingActive(isRatingApplied);
-
+                        //undo the changes
+                        setIsRelevanceActive(isRelevanceApplied);
+                        setIsOpenNowActive(isOpenNowApplied);
+                        setIsPopularActive(isPopularApplied)
+                        setIsRatingActive(isRatingApplied);
+                        setTempMinP(minP);
+                        setTempMaxP(maxP);
+                        setTempSliderValue(sliderValue);
                         setisFiltersModal(!isFiltersModal);
                       }}>
-                    
-                    
-                    
-                    
+          
                       <View className="w-[35] items-center justify-center ">
                         <Text className="text-[25px] buttonTextBlack">
                           X
                         </Text>
                       </View>
+
                   </Pressable>
 
               </View>
               
-              <View className="flex-1 border border-stroke gap-[10]">
+              <View className="flex-1 border border-stroke gap-[5%] ">
                 
-                <Text className="text-[20px] buttonTextBlack ml-[5%] mt-[2%] mb-[2%]">
+                <Text className="text-[20px] buttonTextBlack ml-[5%] mt-[5%]">
                   Sort by
                 </Text>
 
                 <View className="flex-row justify-between ml-[5%] mr-[5%]">
-                  <ToggleButton width={width} text="Relevance" flag={isRelevanceActive} onPress={(newf)=>{setisRelevanceActive(newf)}}/>
-                  <ToggleButton width={width} text="Open Now" flag={isOpenNowActive} onPress={(newf)=>{setisOpenNowActive(newf)}}/>
+                  <ToggleButton width={width} text="Relevance" flag={isRelevanceActive} onPress={(newf)=>{setIsRelevanceActive(newf)}}/>
+                  <ToggleButton width={width} text="Open Now" flag={isOpenNowActive} onPress={(newf)=>{setIsOpenNowActive(newf)}}/>
                 </View>
 
-                <View className="flex-row justify-between ml-[5%] mr-[5%]">
-                  <ToggleButton width={width} text="Popular" flag={isPopularActive} onPress={(newf)=>{setisPopularActive(newf)}}/>
-                  <ToggleButton width={width} text="Rating" flag={isRatingActive} onPress={(newf)=>{setisRatingActive(newf)}}/>
+                <View className="flex-row justify-between ml-[5%] mr-[5%] mt-[-5%]">
+                  <ToggleButton width={width} text="Popular" flag={isPopularActive} onPress={(newf)=>{setIsPopularActive(newf)}}/>
+                  <ToggleButton width={width} text="Rating" flag={isRatingActive} onPress={(newf)=>{setIsRatingActive(newf)}}/>
                 </View>
 
-                <Text className="text-[20px] buttonTextBlack ml-[5%] mt-[2%] mb-[2%]">
+                <Text className="text-[20px] buttonTextBlack ml-[5%] ">
                   Price
                 </Text>
 
-                <View className="flex-row ml-[5%] mr-[5%] gap-[5%] items-center">
+                <View className="flex-row ml-[5%] mr-[5%] gap-[2%] items-center">
                   <Text className="buttonTextBlack ">
                     Minimum:
                   </Text>
 
-                  <View className="border border-textBlack w-[25%] rounded-xl">
-                    <TextInput value={minP} keyboardType='numeric' 
-                              onChangeText={(newP)=>{       
-                                                newP = newP.split('$').join('');
+                  <View className="border border-textBlack w-[30%] rounded-xl">
+                    <TextInput value={tempMinP} keyboardType='numeric'                  
+                              onChangeText={(newP)=>{              
+                                                newP = newP.split('$').join('');  
                                                 if(newP == '')
-                                                  setminP('');
+                                                  setTempMinP('');
                                                 else
-                                                  setminP(`$${newP}`);        
-                                            }}/>
+                                                  setTempMinP(`$${newP}`);        
+                                }}/>
                   </View> 
 
                   <Text className="buttonTextBlack">
                     Maximum:
                   </Text>
 
-                  <View className="border border-textBlack w-[25%] rounded-xl">
-                    <TextInput value={maxP} keyboardType='numeric'
+                  <View className="border border-textBlack w-[30%] rounded-xl">
+                    <TextInput value={tempMaxP} keyboardType='numeric'
                               onChangeText={(newP)=>{ 
                                               newP = newP.split('$').join('');
                                               if (newP == '')
-                                                setmaxP('')
+                                                setTempMaxP('')
                                               else
-                                                setmaxP(`$${newP}`);
+                                                setTempMaxP(`$${newP}`);
                                             }}/>
                   </View> 
 
                 </View>
                 
-                <Text className="text-[20px] buttonTextBlack ml-[5%] mt-[2%] mb-[2%]">
+                <Text className="text-[20px] buttonTextBlack ml-[5%] mt-[2%] ">
                   Distance
                 </Text>
                 
-                <View className="h-[10%] ml-[5%] mr-[5%]">
+                <View className="h-[10%] ml-[5%] mr-[5%] ">
                     <Text className="self-center">
-                      {sliderValue==maxD?`${sliderValue}+`:sliderValue}
+                      {tempSliderValue==maxD?`${tempSliderValue}+`:tempSliderValue} mi
                     </Text>
 
                     <Slider
@@ -167,17 +173,17 @@ export default function Index() {
                           maximumTrackTintColor="#9E9E9E"
                           thumbTintColor="#3A5779"
                           step={1}
-                          value={sliderValue}
-                          onValueChange={(newVal)=>{setSliderValue(newVal)}}
+                          value={tempSliderValue}
+                          onValueChange={(newVal)=>{setTempSliderValue(newVal)}}
                       />
 
                     <View className="flex-row justify-between">
                       <Text >
-                        {minD}
+                        {minD} mi
                       </Text>
                       <Text >
-                        {maxD}+
-                      </Text>
+                        {maxD}+ mi
+                      </Text> 
                     </View>
                 </View>
                                  
@@ -186,23 +192,26 @@ export default function Index() {
           </View>
 
           {/*Apply these filters only when apply button is pressed*/}
-          <View className="items-end mt-[2%] mb-[2%]">
+          <View className="items-end mt-[2%] mb-[2%] mr-[2%]">
             <NormalButton text="Apply" 
                   onClick={()=>{
                                   if(isRelevanceActive || isOpenNowActive || isPopularActive 
-                                    || isRatingActive || minP != ''|| maxP != '' || sliderValue != maxD /2){
+                                    || isRatingActive || tempMinP != ''|| tempMaxP != '' || tempSliderValue != maxD /2){
                                     setisFiltersActive(true)
                                   }
                                   else{
                                     setisFiltersActive(false)
                                   }
-                                  //filter logic goes here
-                                  
-                                  setisRelevanceApplied(isRelevanceActive);
-                                  setisOpenNowApplied(isOpenNowActive);
-                                  setisPopularApplied(isPopularActive);
-                                  setisRatingApplied(isRatingActive);
 
+                                  //filter logic goes here
+
+                                  setIsRelevanceApplied(isRelevanceActive);
+                                  setIsOpenNowApplied(isOpenNowActive);
+                                  setIsPopularApplied(isPopularActive);
+                                  setIsRatingApplied(isRatingActive);
+                                  setminP(tempMinP);
+                                  setmaxP(tempMaxP);
+                                  setSliderValue(tempSliderValue);
                                   setisFiltersModal(!isFiltersModal)
                                 }}/>
           </View>
