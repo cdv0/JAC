a7652bb115d816a436dc227be9e617a4c260bea8
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-22 12:15:27 -0700

    use arrays to track states of buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { images } from "@/constants/images";
 import Slider from '@react-native-community/slider';
+import { router } from "expo-router";
 import { useState } from "react";
 import { Modal, Pressable, ScrollView, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,42 +9,62 @@ import SearchBar from "../components/SearchBar";
 import ToggleButton from "../components/ToggleButton";
 export default function Index() {
 
+  
+  //#region constants
   const [isFiltersActive, setisFiltersActive] = useState(false);
   const [isFiltersModal, setisFiltersModal] = useState(false);
 
-  const [isOilChangeActive, setIsOiltChangeActive] = useState(false);
-  const [isTireActive, setIsTireActive] = useState(false);
-  const [isSmogActive, setIsSmogActive] = useState(false);
-  const [isTransmissionActive, setIsTransmissionActive] = useState(false);
-  const [isWheelActive, setIsWheelActive] = useState(false);
+  //quick Filter
+  //0 -> oil change, 1 -> tire, 2-> Smog, 3->Transmission, 4->Wheel
+  const [quickFilterStates, setQuickFilterStates] = useState(Array(5).fill(false));
 
+  const updateQuickFilterStates = (i:number, value:boolean) =>{
+    setQuickFilterStates(arr => 
+        arr.map((item, index) => (index === i)?value:item
+        )
+      );
+  };
+  
+  //services filter
   const [isServicesActive, setIsServicesActive] = useState(false);
+  const [isServicesModal, setIsServicesModal] = useState(false);
+  const [ActiveTab, setISActiveTab] = useState('1');
 
-  const [isRelevanceActive, setIsRelevanceActive] = useState(false);
-  const [isRelevanceApplied, setIsRelevanceApplied] = useState(false);
+  //expanded Filters
+  //0-> relevance, 1-> open now, 2->popular, 3-> rating
+  const [expFilterStates, setExpFilterStates] = useState(Array(4).fill(false));
 
-  const [isOpenNowActive, setIsOpenNowActive] = useState(false);
-  const [isOpenNowApplied, setIsOpenNowApplied] = useState(false);
+  const updateExpFilterStates = (i:number, value:boolean) =>{
+    setExpFilterStates(arr => 
+        arr.map((item, index) => (index === i)?value:item
+        )
+      );
+  };
 
-  const [isPopularActive, setIsPopularActive] = useState(false); 
-  const [isPopularApplied, setIsPopularApplied] = useState(false); 
-  
-  const [isRatingActive, setIsRatingActive] = useState(false);
-  const [isRatingApplied, setIsRatingApplied] = useState(false);
-  
+  const [expFilterApplied, setExpFilterApplied] = useState(Array(4).fill(false));
+
+  const updateExpFilterApplied = (i:number, value:boolean) =>{
+    setExpFilterApplied(arr => 
+        arr.map((item, index) => (index === i)?value:item
+        )
+      );
+  };
+
   const [minP, setminP] = useState('');
   const [tempMinP, setTempMinP] = useState('');
 
   const [maxP, setmaxP] = useState('');
   const [tempMaxP, setTempMaxP] = useState('');
-
-  const width= '45%';
   const minD = 0;
   const maxD = 20;
   
   const [sliderValue, setSliderValue] = useState(maxD / 2); 
   const [tempSliderValue, setTempSliderValue] = useState(sliderValue);
   const [warning, setWarning] = useState(false);
+  
+  const width= '45%';
+  //#endregion
+
   return (
    <SafeAreaView className="flex-1" edges={['right', 'top', 'left']}>
       <View
@@ -52,19 +73,19 @@ export default function Index() {
         <View className="justify-center w-full h-[17%]">
           <images.searchBackground width="100%" height="100%" style={{ position: 'absolute', zIndex: 0 }} />
           <View className="ml-[50%]">
-            <NormalButton onClick={()=>{}} text={"Enter Garage"}/>
+            <NormalButton onClick={()=>{router.push('/(tabs)/garage')}} text={"Enter Garage"}/>
           </View>
         </View > 
         <SearchBar placeholder1="Search" placeholder2="Location"/>
         <View >
           <ScrollView  horizontal={true} contentContainerStyle={{gap:10}} showsHorizontalScrollIndicator={false}>
             <NormalButton variant={`${isFiltersActive?`primary`:`outline`}`} onClick={()=>{setisFiltersModal(!isFiltersModal)}} text="Filters"/>
-            <NormalButton variant={`${isServicesActive?`primary`:`outline`}`} onClick={()=>{setIsServicesActive(!isServicesActive)}} text="Services"/>
-            <ToggleButton flag = {isOilChangeActive} onPress={(newF)=>{setIsOiltChangeActive(newF)}} text="Oil Change" />
-            <ToggleButton flag = {isTireActive} onPress={(newf)=>{setIsTireActive(newf)}} text="Tire Rotation"/>
-            <ToggleButton flag = {isSmogActive} onPress={(newf)=>{setIsSmogActive(newf)}} text="Smog Check"/>
-            <ToggleButton flag ={isTransmissionActive} onPress={(newf)=>{setIsTransmissionActive(newf)}} text="Transmission Repair"/>
-            <ToggleButton flag = {isWheelActive} onPress={(newf)=>{setIsWheelActive(newf)}} text="Wheel Alignment"/>
+            <NormalButton variant={`${isServicesActive?`primary`:`outline`}`} onClick={()=>{setIsServicesModal(!isServicesModal)}} text="Services"/>
+            <ToggleButton flag = {quickFilterStates[0]} onPress={(newf)=>{updateQuickFilterStates(0,newf)}} text="Oil Change" />
+            <ToggleButton flag = {quickFilterStates[1]} onPress={(newf)=>{updateQuickFilterStates(1,newf)}} text="Tire Rotation"/>
+            <ToggleButton flag = {quickFilterStates[2]} onPress={(newf)=>{updateQuickFilterStates(2, newf)}} text="Smog Check"/>
+            <ToggleButton flag ={quickFilterStates[3]} onPress={(newf)=>{updateQuickFilterStates(3, newf)}} text="Transmission Repair"/>
+            <ToggleButton flag = {quickFilterStates[4]} onPress={(newf)=>{updateQuickFilterStates(4, newf)}} text="Wheel Alignment"/>
           </ScrollView>
         </View>
         
@@ -84,10 +105,10 @@ export default function Index() {
                     onPress={()=> 
                       {
                         //undo the changes
-                        setIsRelevanceActive(isRelevanceApplied);
-                        setIsOpenNowActive(isOpenNowApplied);
-                        setIsPopularActive(isPopularApplied)
-                        setIsRatingActive(isRatingApplied);
+                        updateExpFilterStates(0, expFilterApplied[0]);
+                        updateExpFilterStates(1, expFilterApplied[1]);
+                        updateExpFilterStates(2, expFilterApplied[2])
+                        updateExpFilterStates(3, expFilterApplied[3]);
                         setTempMinP(minP);
                         setTempMaxP(maxP);
                         setWarning(false);
@@ -112,13 +133,13 @@ export default function Index() {
                 </Text>
 
                 <View className="flex-row justify-between ml-[5%] mr-[5%]">
-                  <ToggleButton width={width} text="Relevance" flag={isRelevanceActive} onPress={(newf)=>{setIsRelevanceActive(newf)}}/>
-                  <ToggleButton width={width} text="Open Now" flag={isOpenNowActive} onPress={(newf)=>{setIsOpenNowActive(newf)}}/>
+                  <ToggleButton width={width} text="Relevance" flag={expFilterStates[0]} onPress={(newf)=>{updateExpFilterStates(0, newf)}}/>
+                  <ToggleButton width={width} text="Open Now" flag={expFilterStates[1]} onPress={(newf)=>{updateExpFilterStates(1,newf)}}/>
                 </View>
 
                 <View className="flex-row justify-between ml-[5%] mr-[5%] mt-[-2%]">
-                  <ToggleButton width={width} text="Popular" flag={isPopularActive} onPress={(newf)=>{setIsPopularActive(newf)}}/>
-                  <ToggleButton width={width} text="Rating" flag={isRatingActive} onPress={(newf)=>{setIsRatingActive(newf)}}/>
+                  <ToggleButton width={width} text="Popular" flag={expFilterStates[2]} onPress={(newf)=>{updateExpFilterStates(2, newf)}}/>
+                  <ToggleButton width={width} text="Rating" flag={expFilterStates[3]} onPress={(newf)=>{{updateExpFilterStates(3, newf)}}}/>
                 </View>
 
                 <Text className="text-[20px] buttonTextBlack ml-[5%] ">
@@ -204,8 +225,8 @@ export default function Index() {
                    if(min<=max)
                    {
                       setWarning(false);
-                      if(isRelevanceActive || isOpenNowActive || isPopularActive 
-                        || isRatingActive || tempMinP != ''|| tempMaxP != '' || tempSliderValue != maxD /2){
+                      if(expFilterStates[0] || expFilterStates[1] || expFilterStates[2] 
+                        || expFilterStates[3] || tempMinP != ''|| tempMaxP != '' || tempSliderValue != maxD /2){
                         setisFiltersActive(true)
                       }
                       else{
@@ -214,10 +235,10 @@ export default function Index() {
 
                       //filter logic goes here
 
-                      setIsRelevanceApplied(isRelevanceActive);
-                      setIsOpenNowApplied(isOpenNowActive);
-                      setIsPopularApplied(isPopularActive);
-                      setIsRatingApplied(isRatingActive);
+                      updateExpFilterApplied(0, expFilterStates[0]);
+                      updateExpFilterApplied(1, expFilterStates[1]);
+                      updateExpFilterApplied(2, expFilterStates[2]);
+                      updateExpFilterApplied(3, expFilterStates[3]);
                       setminP(tempMinP);
                       setmaxP(tempMaxP);
                       setSliderValue(tempSliderValue);
@@ -229,6 +250,52 @@ export default function Index() {
                                 
           </View>
 
+        </Modal>
+
+        {/*Expand Services*/}
+        <Modal visible={isServicesModal}>
+          <View className="flex-1">
+              <View className="flex flex-row justify-between ml-[2%] mr-[2%] mt-[5%] mb-[5%]">
+                <Text className="justify-start  text-[25px] buttonTextBlack">
+                  Services
+                </Text>
+
+                <Pressable 
+                  onPress={()=> 
+                    {
+                      setIsServicesModal(!isServicesModal);
+                    }}>
+        
+                    <View className="w-[35] items-center justify-center ">
+                      <Text className="text-[25px] buttonTextBlack">
+                        X
+                      </Text>
+                    </View>
+
+                </Pressable>
+              </View>
+
+              <View className="h-full flex-row border border-stroke">
+                <View className="w-[30%]">
+                  <ScrollView>
+                    
+                    <Text onPress={()=>{setISActiveTab('1')}} 
+                        className={`bg ${ActiveTab == '1'?'bg-stroke':''} py-[10%] buttonTextBlue text-[20px] text-center`}> Hi</Text>
+                    <Text onPress={()=>{setISActiveTab('2')}} 
+                        className={`bg ${ActiveTab == '2'?'bg-stroke':''} py-[10%] buttonTextBlue text-[20px] text-center`}>Hello</Text>
+                    
+  
+                  </ScrollView>
+                </View>
+                
+
+                <View className="w-full border border-stroke">
+                    <Text>
+                      Hi
+                    </Text>
+                </View>
+              </View>
+          </View>
         </Modal>
         
       </View>
