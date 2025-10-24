19a62d3878cb74b302c441eab5a6eae7052a666a
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-24 15:17:19 -0700

    use useStates() to update states for each array states. Prevent non-numerical values from being entered for prices

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,6 @@ import SearchBar from "../components/SearchBar";
 import ToggleButton from "../components/ToggleButton";
 export default function Index() {
 
-  //TODO create a string array of categories to filter
   const [categories, setCategories] = useState<string[]>([]);
 
   //#region helper functions
@@ -28,7 +27,13 @@ export default function Index() {
       insertCategory(Category);
     else
       removeCategory(Category)
-  }
+  };
+
+    const updateStates =(i:number, value:boolean, setFunc:React.Dispatch<React.SetStateAction<any[]>>) =>{
+    setFunc(arr =>
+      arr.map((item, index) =>(index ===i)?value:item)
+    );
+  };
 
   //#region constants
   const [isFiltersActive, setisFiltersActive] = useState(false);
@@ -39,24 +44,12 @@ export default function Index() {
   //0 -> oil change, 1 -> tire, 2-> Smog, 3->Transmission, 4->Wheel
   const [quickFilterStates, setQuickFilterStates] = useState(Array(5).fill(false));
 
-  const updateQuickFilterStates = (i:number, value:boolean) =>{
-    setQuickFilterStates(arr => 
-        arr.map((item, index) => (index === i)?value:item
-        )
-      );
-  };
   //#endregion
   
   //#region services filter
   const [isServicesActive, setIsServicesActive] = useState(false);
   const [isServicesModal, setIsServicesModal] = useState(false);
   const [ActiveTab, setISActiveTab] = useState('1');
-
-  const updateServices =(i:number, value:boolean, setFunc:React.Dispatch<React.SetStateAction<any[]>>) =>{
-    setFunc(arr =>
-      arr.map((item, index) =>(index ===i)?value:item)
-    );
-  };
 
   const [AC_Heat, setAC_Heat] = useState(Array(5).fill(false));
   const [Bat_Elec, setBat_Elec] = useState(Array(5).fill(false));
@@ -67,25 +60,25 @@ export default function Index() {
           <View>
             <ScrollView className="gap-[5%]">
               <Text className={` ${AC_Heat[0]?'buttonTextWhite bg-primaryBlue':'buttonTextBlue'} w-full border-b-[2px] py-[5%] border-stroke text-center`}
-                    onPress={()=>{updateServices(0, !AC_Heat[0], setAC_Heat)}}
+                    onPress={()=>{updateStates(0, !AC_Heat[0], setAC_Heat)}}
               >
                 AC recharge
               </Text>
               <Text className={` ${AC_Heat[1]?'buttonTextWhite bg-primaryBlue':'buttonTextBlue'} w-full border-b-[2px] py-[5%] border-stroke text-center`}
-                    onPress={()=>{updateServices(1, !AC_Heat[1], setAC_Heat)}}
+                    onPress={()=>{updateStates(1, !AC_Heat[1], setAC_Heat)}}
               >
                 Compressor replacement
               </Text>
               <Text className={` ${AC_Heat[2]?'buttonTextWhite bg-primaryBlue':'buttonTextBlue'} w-full border-b-[2px] py-[5%] border-stroke text-center`}
-                    onPress={()=>{updateServices(2, !AC_Heat[2], setAC_Heat)}}>
+                    onPress={()=>{updateStates(2, !AC_Heat[2], setAC_Heat)}}>
                 Radiator service
               </Text>
               <Text className={` ${AC_Heat[3]?'buttonTextWhite bg-primaryBlue':'buttonTextBlue'} w-full border-b-[2px] py-[5%] border-stroke text-center`}
-                    onPress={()=>{updateServices(3, !AC_Heat[3], setAC_Heat)}}>
+                    onPress={()=>{updateStates(3, !AC_Heat[3], setAC_Heat)}}>
                 Heater Core repair
               </Text>
               <Text className={` ${AC_Heat[4]?'buttonTextWhite bg-primaryBlue':'buttonTextBlue'} w-full border-b-[2px] py-[5%] border-stroke text-center`}
-                    onPress={()=>{updateServices(4, !AC_Heat[4], setAC_Heat)}}>
+                    onPress={()=>{updateStates(4, !AC_Heat[4], setAC_Heat)}}>
                 Thermostat/water pump
               </Text>
             </ScrollView>
@@ -95,7 +88,7 @@ export default function Index() {
         return(
           <ScrollView className="gap-[5%]">
             <Text className={` ${Bat_Elec[0]?'buttonTextWhite bg-primaryBlue':'buttonTextBlue'} w-full border-b-[2px] py-[5%] border-stroke text-center`}
-                    onPress={()=>{updateServices(0, !Bat_Elec[0], setBat_Elec)}}>
+                    onPress={()=>{updateStates(0, !Bat_Elec[0], setBat_Elec)}}>
               Battery Replacement
             </Text>
             <Text className="buttonTextBlue w-full border-b py-[5%] border-stroke text-center">
@@ -127,21 +120,7 @@ export default function Index() {
   //0-> relevance, 1-> open now, 2->popular, 3-> rating
   const [expFilterStates, setExpFilterStates] = useState(Array(4).fill(false));
 
-  const updateExpFilterStates = (i:number, value:boolean) =>{
-    setExpFilterStates(arr => 
-        arr.map((item, index) => (index === i)?value:item
-        )
-      );
-  };
-
   const [expFilterApplied, setExpFilterApplied] = useState(Array(4).fill(false));
-
-  const updateExpFilterApplied = (i:number, value:boolean) =>{
-    setExpFilterApplied(arr => 
-        arr.map((item, index) => (index === i)?value:item
-        )
-      );
-  };
 
   const [minP, setminP] = useState('');
   const [tempMinP, setTempMinP] = useState('');
@@ -176,11 +155,11 @@ export default function Index() {
           <ScrollView  horizontal={true} contentContainerStyle={{gap:10}} showsHorizontalScrollIndicator={false}>
             <NormalButton variant={`${isFiltersActive?`primary`:`outline`}`} onClick={()=>{setisFiltersModal(!isFiltersModal)}} text="Filters"/>
             <NormalButton variant={`${isServicesActive?`primary`:`outline`}`} onClick={()=>{setIsServicesModal(!isServicesModal)}} text="Services"/>
-            <ToggleButton flag = {quickFilterStates[0]} onPress={(newf)=>{updateQuickFilterStates(0,newf)}} text="Oil Change" />
-            <ToggleButton flag = {quickFilterStates[1]} onPress={(newf)=>{updateQuickFilterStates(1,newf)}} text="Tire Rotation"/>
-            <ToggleButton flag = {quickFilterStates[2]} onPress={(newf)=>{updateQuickFilterStates(2, newf)}} text="Smog Check"/>
-            <ToggleButton flag ={quickFilterStates[3]} onPress={(newf)=>{updateQuickFilterStates(3, newf)}} text="Transmission Repair"/>
-            <ToggleButton flag = {quickFilterStates[4]} onPress={(newf)=>{updateQuickFilterStates(4, newf)}} text="Wheel Alignment"/>
+            <ToggleButton flag = {quickFilterStates[0]} onPress={(newf)=>{updateStates(0, newf, setQuickFilterStates)}} text="Oil Change" />
+            <ToggleButton flag = {quickFilterStates[1]} onPress={(newf)=>{updateStates(1, newf, setQuickFilterStates)}} text="Tire Rotation"/>
+            <ToggleButton flag = {quickFilterStates[2]} onPress={(newf)=>{updateStates(2, newf, setQuickFilterStates)}} text="Smog Check"/>
+            <ToggleButton flag ={quickFilterStates[3]} onPress={(newf)=>{updateStates(3, newf, setQuickFilterStates)}} text="Transmission Repair"/>
+            <ToggleButton flag = {quickFilterStates[4]} onPress={(newf)=>{updateStates(4, newf, setQuickFilterStates)}} text="Wheel Alignment"/>
           </ScrollView>
         </View>
         
@@ -200,10 +179,10 @@ export default function Index() {
                     onPress={()=> 
                       {
                         //undo the changes
-                        updateExpFilterStates(0, expFilterApplied[0]);
-                        updateExpFilterStates(1, expFilterApplied[1]);
-                        updateExpFilterStates(2, expFilterApplied[2])
-                        updateExpFilterStates(3, expFilterApplied[3]);
+                        updateStates(0, expFilterApplied[0], setExpFilterStates) 
+                        updateStates(1, expFilterApplied[1], setExpFilterStates);
+                        updateStates(2, expFilterApplied[2], setExpFilterStates)
+                        updateStates(3, expFilterApplied[3], setExpFilterStates);
                         setTempMinP(minP);
                         setTempMaxP(maxP);
                         setWarning(false);
@@ -228,13 +207,13 @@ export default function Index() {
                 </Text>
 
                 <View className="flex-row justify-between ml-[5%] mr-[5%]">
-                  <ToggleButton width={width} text="Relevance" flag={expFilterStates[0]} onPress={(newf)=>{updateExpFilterStates(0, newf)}}/>
-                  <ToggleButton width={width} text="Open Now" flag={expFilterStates[1]} onPress={(newf)=>{updateExpFilterStates(1,newf)}}/>
+                  <ToggleButton width={width} text="Relevance" flag={expFilterStates[0]} onPress={(newf)=>{updateStates(0, newf, setExpFilterStates)}}/>
+                  <ToggleButton width={width} text="Open Now" flag={expFilterStates[1]} onPress={(newf)=>{updateStates(1, newf, setExpFilterStates)}}/>
                 </View>
 
                 <View className="flex-row justify-between ml-[5%] mr-[5%] mt-[-2%]">
-                  <ToggleButton width={width} text="Popular" flag={expFilterStates[2]} onPress={(newf)=>{updateExpFilterStates(2, newf)}}/>
-                  <ToggleButton width={width} text="Rating" flag={expFilterStates[3]} onPress={(newf)=>{{updateExpFilterStates(3, newf)}}}/>
+                  <ToggleButton width={width} text="Popular" flag={expFilterStates[2]} onPress={(newf)=>{updateStates(2, newf, setExpFilterStates)}}/>
+                  <ToggleButton width={width} text="Rating" flag={expFilterStates[3]} onPress={(newf)=>{updateStates(3, newf, setExpFilterStates)}}/>
                
                 </View>
 
@@ -249,8 +228,8 @@ export default function Index() {
                   <View className={`border ${warning?'border-dangerBrightRed':'border-textBlack'} w-[30%] rounded-xl`}>
                     <TextInput value={tempMinP} keyboardType='numeric'                  
                               onChangeText={(newP)=>{              
-                                                newP = newP.split('$').join('');
-                                                newP = newP.replace(/[-,.]/g,'');
+                                                //newP = newP.split('$').join('');
+                                                newP = newP.replace(/[^0-9]/g,'');
                                                 if(newP == '')
                                                   setTempMinP('');
                                                 else
@@ -265,8 +244,8 @@ export default function Index() {
                   <View className={`border ${warning?'border-dangerBrightRed':'border-textBlack'} w-[30%] rounded-xl`}>
                     <TextInput value={tempMaxP} keyboardType='numeric'
                               onChangeText={(newP)=>{ 
-                                              newP = newP.split('$').join('');
-                                              newP = newP.replace(/[-,.]/g,'');
+                                              //newP = newP.split('$').join('');
+                                              newP = newP.replace(/[^0-9]/g,'');
                                               if (newP == '')
                                                 setTempMaxP('')
                                               else
@@ -318,7 +297,7 @@ export default function Index() {
                   onClick={()=>{
                    let min = (tempMinP.split('$').join('')=='')?null: parseInt(tempMinP.split('$').join(''), 10);
                    let max = (tempMaxP.split('$').join('')=='')?null: parseInt(tempMaxP.split('$').join(''), 10);
-                   {/*TODO handle when min =='' and max!='' and vice versa*/}
+                   
                    if(min==null || max ==null ||min<=max)
                    {
                       setWarning(false);
@@ -331,10 +310,10 @@ export default function Index() {
 
                       //filter logic goes here
 
-                      updateExpFilterApplied(0, expFilterStates[0]);
-                      updateExpFilterApplied(1, expFilterStates[1]);
-                      updateExpFilterApplied(2, expFilterStates[2]);
-                      updateExpFilterApplied(3, expFilterStates[3]);
+                      updateStates(0, expFilterStates[0], setExpFilterApplied);
+                      updateStates(1, expFilterStates[1], setExpFilterApplied);
+                      updateStates(2, expFilterStates[2], setExpFilterApplied);
+                      updateStates(3, expFilterStates[3], setExpFilterApplied);
                       setminP(tempMinP);
                       setmaxP(tempMaxP);
                       setSliderValue(tempSliderValue);
@@ -420,11 +399,6 @@ export default function Index() {
                     
                 </View>
               </View>
-            <View className="border border-stroke py-[2%]">
-              <View className="self-end mr-[5%]">
-                <NormalButton text="Apply" onClick={() => {}}/>
-              </View>
-            </View>
           </View>
         </Modal>
         
