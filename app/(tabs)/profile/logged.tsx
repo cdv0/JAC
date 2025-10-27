05d8db50e580b97c64489859609dbfe4ba949334
Allen Dang <akdang592@gmail.com>
2025-10-27 15:54:44 -0700

    Added Account Icon, Initials, Name, CreationDate

--- app/(tabs)/profile/logged.tsx
+++ app/(tabs)/profile/logged.tsx
@@ -3,7 +3,7 @@ import { fetchUserAttributes, getCurrentUser } from 'aws-amplify/auth'
 import { useRouter } from 'expo-router'
 import { useEffect, useState } from 'react'
 import { Pressable, ScrollView, Text, View } from 'react-native'
-import { Cog6ToothIcon, UserCircleIcon } from 'react-native-heroicons/solid'
+import { Cog6ToothIcon } from 'react-native-heroicons/solid'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export const account = () => {
@@ -58,7 +58,7 @@ export const account = () => {
             <Cog6ToothIcon size={32} color="#000" />
           </Pressable>
         </View>
-        <View className="relative items-center justify-center bg-white h-52">
+        <View className="flex-row relative items-center justify-center bg-white h-52">
           {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             height="200px"
@@ -68,11 +68,13 @@ export const account = () => {
           >
             <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
           </svg> */}
-          <UserCircleIcon size={200} color="#e3e3e3" />
-          <Text>
-            {firstName}
-            {lastName} test
-          </Text>
+          <View className="mx-5 mt-6 h-32 w-32 rounded-full bg-accountOrange items-center justify-center">
+            <Text className="mx-5 mt-6 text-white font-bold">{fullInitials}</Text>
+          </View>
+          <View className="flex-col">
+            <Text>{firstName} {lastName}</Text>
+            <Text className="text-sm text-gray-500">Member since {createdAt?.slice(0, 10)}</Text>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
