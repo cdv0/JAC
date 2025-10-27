3cb5b9a4424165a765ea47d159c6115ff567e574
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-27 14:30:26 -0700

    Modified to enable dynamic routing and added touchable opacity

--- app/components/MechanicView.tsx
+++ app/components/MechanicView.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Text, TouchableOpacity, View, ImageSourcePropType, Image } from 'react-native'
+import { Link } from 'expo-router'
+import { Image, ImageSourcePropType, Text, TouchableOpacity, View } from 'react-native'
 
 
 type MechanicViewProps = {
@@ -16,14 +16,18 @@ export default function MechanicView(
         name, type, rating, reviews, image
     }: MechanicViewProps){
         return(
-            <View className = {`flex flex-row`}>
-                <Image className ={"w-3/12 m-2 object-cover" }source = {image}></Image>
-                <View className = {`flex flex-col`}>
-                    <Text className = {`text-xl text-textBlack`}>{name}</Text>
-                    <Text className = {`text-l text-subheaderGray`}>{type}</Text>
-                    <Text>{rating}/5</Text>
-                    <Text>Reviews: {reviews}</Text>
-                </View>
-            </View>
+            <Link href={`../mechanic/${name}`} asChild>
+                <TouchableOpacity>
+                   <View className = {`flex flex-row`}>
+                    <Image className ={"w-3/12 m-2 object-cover" }source = {image}></Image>
+                    <View className = {`flex flex-col`}>
+                        <Text className = {`text-xl text-textBlack`}>{name}</Text>
+                        <Text className = {`text-l text-subheaderGray`}>{type}</Text>
+                        <Text>{rating}/5</Text>
+                        <Text>Reviews: {reviews}</Text>
+                    </View>
+                </View> 
+                </TouchableOpacity>    
+            </Link>
         )
 }
