3d1963c0a6785321181265b652315ab8817c076f
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-10-27 22:20:56 -0700

    added default mechanic image

--- constants/images.ts
+++ constants/images.ts
@@ -1,8 +1,10 @@
+import defaultImage from "@/public/assets/images/default_image.svg";
 import groupLogo from "@/public/assets/images/group-name.svg";
 import logo from "@/public/assets/images/logo.svg";
 import searchBackground from "@/public/assets/images/searchBG.svg";
 export const images = {
     logo,
     groupLogo,
-    searchBackground
+    searchBackground,
+    defaultImage
 };
