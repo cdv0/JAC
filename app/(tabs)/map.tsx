766a6d47a46d173fb6d15a3611b089bb9c3f1872
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-11-14 16:14:21 -0800

    removed

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,16 +1,13 @@
-import { StyleSheet, View } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import SearchBar from '../components/SearchBar';
 const map = () => {
   return (
     <SafeAreaView className='flex-1' edges={['right', 'top', 'left']}>
-      <View className='flex-1'>
+      {/* <View className='flex-1'>
         <View style={{zIndex:1}}>
           <SearchBar placeholder1='Search' placeholder2='Location'/>
         </View>
         <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_GOOGLE}/>
-      </View>
+      </View> */}
     </SafeAreaView>
   )
 }
