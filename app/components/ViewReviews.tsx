c8c877d92f477241ef50320fbd203dcb8ffd2a11
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-11-16 13:16:45 -0800

    Implemented how to render views

--- app/components/ViewReviews.tsx
+++ app/components/ViewReviews.tsx
@@ -1,9 +1,33 @@
 import { Text, View } from 'react-native'
-
-const ViewReviews = () => {
+import { StarRatingDisplay } from 'react-native-star-rating-widget'
+import { Float } from 'react-native/Libraries/Types/CodegenTypesNamespace'
+type ReviewProps ={
+    Rating: Float,
+    Review: String,
+    userId: string
+}
+const ViewReviews = ({Rating, Review, userId}:ReviewProps) => {
   return (
-    <View>
-      <Text>ViewReviews</Text>
+    <View className='flex-1 border-b-2 border-stroke'>
+      <View className='flex-row ml-5'>
+        <View className='w-[50] h-[50] border border-black rounded-full justify-center items-center'>
+          <Text className='buttonTextBlack'>
+            T
+          </Text>
+        </View>
+        <View>
+          <View className='ml-5'>
+            <Text className='ml-2'>
+              User {userId}
+            </Text>
+            <StarRatingDisplay color={'black'} starSize={16} starStyle={{width:4}} style={{ alignItems:'center'}} rating={Rating}/>
+          </View>
+        </View>
+          
+      </View>
+      <Text className='buttonTextBlack mx-5'>
+        {Review}
+      </Text>
     </View>
   )
 }
