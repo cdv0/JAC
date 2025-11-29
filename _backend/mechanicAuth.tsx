286b819f0b55543b97f931c14bd1bed51ba064c5
Jayden Tran <111601544+jtran101@users.noreply.github.com>
2025-11-29 11:59:58 -0800

    Added Mechanic signUp and confirmSignUp functions

---
+++ _backend/mechanicAuth.tsx
@@ -0,0 +1,38 @@
+import {
+  CognitoIdentityProvider,
+  ConfirmSignUpCommand,
+  SignUpCommand,
+} from '@aws-sdk/client-cognito-identity-provider'
+
+const REGION = 'us-west-2'
+
+const cognitoClient = new CognitoIdentityProvider({ region: REGION })
+
+let mechanicSession: {
+  accessToken?: string
+  idToken?: string
+  refreshToken?: string
+  username?: string
+} = {}
+
+export async function mechanicSignUp(email: string, password: string) {
+  const command = new SignUpCommand({
+    ClientId: process.env.MECHANIC_CLIENT_ID,
+    Username: email,
+    Password: password,
+  })
+
+  const response = await cognitoClient.send(command)
+
+  return response
+}
+
+export async function mechanicConfirmSignUp(email: string, code: string) {
+  const command = new ConfirmSignUpCommand({
+    ClientId: process.env.MECHANIC_CLIENT_ID,
+    Username: email,
+    ConfirmationCode: code,
+  })
+
+  return cognitoClient.send(command)
+}
