0684379b615c749af31cdd5ad163f0aa98b19e7e
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-12-03 00:24:28 -0800

    button to test add shop

--- app/(tabs)/profile/mechanic/mechanicSignIn.tsx
+++ app/(tabs)/profile/mechanic/mechanicSignIn.tsx
@@ -1,13 +1,15 @@
+import NormalButton from '@/app/components/NormalButton'
 import { useState } from 'react'
 import { Text, View } from 'react-native'
+import AddShop from './AddShop'
 const mechanicSignIn = () => {
   const [addModalVisible, setAddModalVisible] = useState(false)
   const close =()=>{setAddModalVisible(false)};
   return (
     <View className='flex-1 bg-white'>
-      {/* <NormalButton onClick={()=>{setAddModalVisible(true)}} text='Add'/> */} 
+      <NormalButton onClick={()=>{setAddModalVisible(true)}} text='Add'/>
       <Text>Mechanic Sign In</Text>
-      {/* <AddShop visible={addModalVisible} onClose={close}/> */}
+      <AddShop visible={addModalVisible} onClose={close}/>
     </View>
   )
 }
