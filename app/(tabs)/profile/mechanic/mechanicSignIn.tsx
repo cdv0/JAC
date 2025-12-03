f3a737b090cb3a9e4517bf7553afc1e8694e735e
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-12-03 08:31:22 -0800

    removed button

--- app/(tabs)/profile/mechanic/mechanicSignIn.tsx
+++ app/(tabs)/profile/mechanic/mechanicSignIn.tsx
@@ -1,15 +1,8 @@
-import AddShop from '@/app/components/AddShop'
-import NormalButton from '@/app/components/NormalButton'
-import { useState } from 'react'
 import { Text, View } from 'react-native'
 const mechanicSignIn = () => {
-  const [addModalVisible, setAddModalVisible] = useState(false)
-  const close =()=>{setAddModalVisible(false)};
   return (
     <View className='flex-1 bg-white'>
-      <NormalButton onClick={()=>{setAddModalVisible(true)}} text='Add'/>
       <Text>Mechanic Sign In</Text>
-      <AddShop visible={addModalVisible} onClose={close}/>
     </View>
   )
 }
